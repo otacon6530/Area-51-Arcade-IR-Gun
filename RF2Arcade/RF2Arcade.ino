0df05e3f41a6a38a3e082dfe1e75f338fab8deef
James Stephens <onlineworkmail@gmail.com>
2024-09-11 22:01:15 -0400

    Cleaning up

--- RF2Arcade/RF2Arcade.ino
+++ RF2Arcade/RF2Arcade.ino
@@ -34,23 +34,26 @@ protected:
 #include "DFRobotIRPosition.h"
 DFRobotIRPosition myDFRobotIRPosition;
 
-float coords[4][2];
-double rotX[4];    ///< Store the X position
-double rotY[4];    ///< Store the Y position
-int cameraCenter = 512;
-float xPerc = 0;
-float yPerc = 0;
+struct {
+              float coords[4][2];
+              double sides[4];
+              int cameraCenter=512;
+              float x = 0;
+              float y= 0;
+            }beacon;
+struct {
+              
+              int width = 47;
+              int height = 239;
+              int x = 25;
+              int y = 125;
+              int vSyncOffset=240;
+              int hSyncCounter=0;
+            } screen;
 
-int maxX = 47;
-int maxY = 239;
-int x = 25;
-int y = 135;
-
-const int digitalPin = 7;  // Digital output pin for the LED
+//Define used pins
+const int digitalPin = 7; 
 const int firePin = 6;     // Digital output pin for the fire action
-
-int counter = 0;
-
 const byte tempTrig = 4;
 const byte vSyncPin = 3;
 const byte hSyncPin = 2;
@@ -66,35 +69,35 @@ void MouseRptParser::OnMouseMove(MOUSEINFO *mi)
     int dX = (mi->dX < -3) ? -1 : (mi->dX > 3) ? 1 : 0;
     int dY = (mi->dY < -1) ? -4 : (mi->dY > 1) ? 4 : 0;
 
-    x = constrain(x + dX, 3, 49);
-    y = constrain(y + dY, 0, 263);
+    screen.x = constrain(screen.x + dX, 3, 49);
+    screen.y = constrain(screen.y + dY, 0, 263);
 }
 
 void MouseRptParser::OnLeftButtonUp(MOUSEINFO *mi) {}
 void MouseRptParser::OnLeftButtonDown(MOUSEINFO *mi)
 {
-    Serial.println("L Butt Dn");
+    //USB Left mouse button will fire gun.
     pinMode(firePin, OUTPUT);
     digitalWrite(firePin, LOW);
-    delay(50);
+    delay(17);
     pinMode(firePin, INPUT);
 }
 
 void MouseRptParser::OnRightButtonUp(MOUSEINFO *mi) {}
 void MouseRptParser::OnRightButtonDown(MOUSEINFO *mi)
 {
-    int tempX = x;
-    int tempY = y;
-    x = 0;
-    y = 0;
-    delay(500);
+    int tempX = screen.x;
+    int tempY = screen.y;
+    screen.x = -262;
+    screen.y = -262;
+    delay(50);
     pinMode(firePin, OUTPUT);
     digitalWrite(firePin, LOW);
     delay(17);
     pinMode(firePin, INPUT);
     delay(100);
-    x = tempX;
-    y = tempY;
+    screen.x = tempX;
+    screen.y = tempY;
 }
 
 void MouseRptParser::OnMiddleButtonUp(MOUSEINFO *mi) {}
@@ -112,11 +115,13 @@ void setup()
 
     myDFRobotIRPosition.begin();
 
+    //Interrupt for Vertical and Horizontal sync.
     pinMode(vSyncPin, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(vSyncPin), vSync, FALLING);
-
     pinMode(hSyncPin, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(hSyncPin), hSync, RISING);
+
+    pinMode(tempTrig, INPUT_PULLUP);
 
     if (Usb.Init() == -1) {
         Serial.println("USB Initialization failed!");
@@ -125,36 +130,35 @@ void setup()
 
     delay(200);
     HidMouse.SetReportParser(0, &Prs);
-    pinMode(tempTrig, INPUT_PULLUP);
 }
 
 void vSync()
 {
-    counter = 240;
+    screen.hSyncCounter = screen.vSyncOffset;
 }
 
 void hSync()
 {
-    counter++;
-    if (counter == y) {
-        delayMicroseconds(x);
+    screen.hSyncCounter++;
+    if (screen.hSyncCounter == screen.y) {
+        delayMicroseconds(screen.x);
         pinMode(digitalPin, OUTPUT);
         digitalWrite(digitalPin, LOW);
         pinMode(digitalPin, INPUT);
     }
-    if (counter >= 262) {
-        counter = 0;
+    if (screen.hSyncCounter >= screen.height) {
+        screen.hSyncCounter = 0;
     }
 }
 
 void test()
 {
     delay(10);
-    y++;
-    if (x == 48) x = 0;
-    if (y >= 262) {
-        y = 0;
-        x++;
+    screen.y++;
+    if (screen.x == screen.width) screen.x = 0;
+    if (screen.y >= screen.height) {
+        screen.y = 0;
+        screen.x++;
     }
 }
 // Function to calculate the slope between two points
@@ -169,18 +173,18 @@ float calculateAngle(float x1, float y1, float x2, float y2) {
 }
 
 // Function to rotate a point around a specific point (px, py) by an angle
-void rotatePoint(float &x, float &y, float angle, float px, float py) {
+void rotatePoint(float &xx, float &yy, float angle, float px, float py) {
     // Translate point to origin
-    float xTranslated = x - px;
-    float yTranslated = y - py;
+    float xTranslated = xx - px;
+    float yTranslated = yy - py;
 
     // Perform the rotation
     float xNew = xTranslated * cos(angle) - yTranslated * sin(angle);
     float yNew = xTranslated * sin(angle) + yTranslated * cos(angle);
 
     // Translate point back
-    x = xNew + px;
-    y = yNew + py;
+    xx = xNew + px;
+    yy = yNew + py;
 }
 
 // Function to ensure the long sides of the rectangle are horizontal
@@ -224,53 +228,53 @@ void IRPosition()
             int yQuadrant = (RFy > 512) ? 1 : -1;
 
             if (xQuadrant == -1 && yQuadrant == 1) {
-                coords[topLeft][0] = RFx;
-                coords[topLeft][1] = RFy;
+                beacon.coords[topLeft][0] = RFx;
+                beacon.coords[topLeft][1] = RFy;
             } else if (xQuadrant == 1 && yQuadrant == 1) {
-                coords[topRight][0] = RFx;
-                coords[topRight][1] = RFy;
+                beacon.coords[topRight][0] = RFx;
+                beacon.coords[topRight][1] = RFy;
             } else if (xQuadrant == -1 && yQuadrant == -1) {
-                coords[bottomLeft][0] = RFx;
-                coords[bottomLeft][1] = RFy;
+                beacon.coords[bottomLeft][0] = RFx;
+                beacon.coords[bottomLeft][1] = RFy;
             } else if (xQuadrant == 1 && yQuadrant == -1) {
-                coords[bottomRight][0] = RFx;
-                coords[bottomRight][1] = RFy;
+                beacon.coords[bottomRight][0] = RFx;
+                beacon.coords[bottomRight][1] = RFy;
             }
         }
 
-        float topM = calculateSlope(coords[topLeft][0],coords[topLeft][1],coords[topRight][0],coords[topRight][1]);
+        float topM = calculateSlope(beacon.coords[topLeft][0],beacon.coords[topLeft][1],beacon.coords[topRight][0],beacon.coords[topRight][1]);
 
-        if (coords[topLeft][0] >= 1023 || coords[bottomRight][0] >= 1023 || coords[bottomLeft][0] >= 1023 || coords[topRight][0] >= 1023) {
-            y = -262;
-            x = -262;
+        if (beacon.coords[topLeft][0] >= 1023 || beacon.coords[bottomRight][0] >= 1023 || beacon.coords[bottomLeft][0] >= 1023 || beacon.coords[topRight][0] >= 1023) {
+            screen.y = -screen.height;
+            screen.x = -screen.width;
         } else {
             // Align the rectangle
             for (int i = 0; i < 4; i++) {
-                float px = coords[i][0];
-                float py = coords[i][1];
+                float px = beacon.coords[i][0];
+                float py = beacon.coords[i][1];
                 float s = sin(atan(-topM));
                 float c = cos(atan(-topM));
 
-                px -= cameraCenter;
-                py -= cameraCenter;
+                px -= beacon.cameraCenter;
+                py -= beacon.cameraCenter;
 
                 float xnew = px * c - py * s;
                 float ynew = px * s + py * c;
 
-                coords[i][0] = xnew + cameraCenter;
-                coords[i][1] = ynew + cameraCenter;
+                beacon.coords[i][0] = xnew + beacon.cameraCenter;
+                beacon.coords[i][1] = ynew + beacon.cameraCenter;
             }
 
-            if (coords[topLeft][0] < 1023 && coords[topRight][0] < 1023) {
-                xPerc = float(cameraCenter - coords[topLeft][0]) / (coords[topRight][0] - coords[topLeft][0]);
+            if (beacon.coords[topLeft][0] < 1023 && beacon.coords[topRight][0] < 1023) {
+                beacon.x = float(beacon.cameraCenter - beacon.coords[topLeft][0]) / (beacon.coords[topRight][0] - beacon.coords[topLeft][0]);
             }
-            x = maxX * (1 - xPerc);
+            screen.x = screen.width * (1 - beacon.x);
 
-            if (coords[topLeft][0] < 1023 && coords[bottomLeft][0] < 1023) {
-                yPerc = float(coords[topLeft][1] - cameraCenter) / (coords[topLeft][1] - coords[bottomLeft][1])+0.10;
+            if (beacon.coords[topLeft][0] < 1023 && beacon.coords[bottomLeft][0] < 1023) {
+                beacon.y = float(beacon.coords[topLeft][1] - beacon.cameraCenter) / (beacon.coords[topLeft][1] - beacon.coords[bottomLeft][1])+0.10;
             }
-            y = maxY * (1 - yPerc);
-            printResult(xPerc,yPerc);
+            screen.y = screen.height * (1 - beacon.y);
+            printResult(beacon.x,beacon.y);
         }
     } else {
         Serial.println("Device not available!");
@@ -310,29 +314,23 @@ void printResult(float xPerc, float yPerc)
 {
     Serial.print("IR Positions:\t");
     for (int i = 0; i < 4; i++) {
-        Serial.print(coords[i][0]);
+        Serial.print(beacon.coords[i][0]);
         Serial.print("\t,");
-        Serial.print(coords[i][1]);
+        Serial.print(beacon.coords[i][1]);
         Serial.print(";\t");
     }
     Serial.print("Rotated:\t");
-    for (int i = 0; i < 4; i++) {
-        Serial.print(rotX[i]);
-        Serial.print("\t,");
-        Serial.print(rotY[i]);
-        Serial.print(";\t");
-    }
     Serial.print("Resolution: ");
-    Serial.print(coords[1][0] - coords[0][0]);
+    Serial.print(beacon.coords[1][0] - beacon.coords[0][0]);
     Serial.print(",");
-    Serial.print(coords[0][1] - coords[2][1]);
+    Serial.print(beacon.coords[0][1] - beacon.coords[2][1]);
     Serial.print("\tPerc:");
-    Serial.print(xPerc);
+    Serial.print(beacon.x);
     Serial.print(",");
-    Serial.print(yPerc);
+    Serial.print(beacon.y);
     Serial.print(";\tPosition:");
-    Serial.print(x);
+    Serial.print(screen.x);
     Serial.print(",");
-    Serial.print(y);
+    Serial.print(screen.y);
     Serial.println();
 }
